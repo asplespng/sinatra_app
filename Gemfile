d61e2d970fd91a9ae0c719224baae72f507a3276
Per Evensen <per.evensen@gmail.com>
2016-05-19 20:36:43 -0400

    basic authenticity

--- Gemfile
+++ Gemfile
@@ -1,13 +1,11 @@
 source 'https://rubygems.org'
-
+gem 'sinatra'
 gem 'activerecord'
-
 gem 'sinatra-activerecord'
-
 gem 'sinatra-contrib'
-
 gem 'sqlite3'
-
 gem 'rake'
-
 gem 'haml'
+gem 'bcrypt'
+
+
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/activerecord'
+require 'bcrypt'
 
 set :database, "sqlite3:sinatra_app_dev.sqlite3"
 
@@ -16,6 +17,13 @@ end
 #   "Hello #{name}"
 # end
 
+enable :sessions
+helpers do
+  def logged_in?
+    session['user_id'].present?
+  end
+
+end
 get '/users' do
   @users = User.all
   haml :'users/index'
@@ -26,18 +34,38 @@ get '/users/new' do
 end
 
 post '/users' do
-    email = params[:email]
-    User.create(email: email)
-    redirect :users
+    user = User.new(email: params['email'], password: params['password'])
+    if user.save
+      redirect '/users'
+    else
+      redirect '/users/new'
+    end
 end
 
-get '/users/:id/edit' do
-  @user = User.find(params[:id])
+get '/users/:id/edit' do |id|
+  @user = User.find(id)
   haml :'users/edit'
 end
 
 put '/users/:id' do
   @user = User.find(params[:id])
   @user.update!(email: params[:email])
-  redirect :users
+  redirect '/users'
+end
+
+get '/sessions/new' do
+  haml :'/sessions/new'
+end
+
+post '/sessions' do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    redirect '/sessions/success'
+  else
+    redirect '/sessions/new'
+  end
+end
+
+get '/sessions/success' do
+  haml :'/sessions/success'
 end
