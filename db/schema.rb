d8e7cb27e54ab7e3b195ae4d7909089d702969b4
Per Evensen <per.evensen@gmail.com>
2016-05-19 20:36:43 -0400

    basic authenticity

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510000850) do
+ActiveRecord::Schema.define(version: 20160514192342) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "password_digest"
   end
 
 end
