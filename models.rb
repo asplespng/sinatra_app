c48da67445a24eea0558c16bd6dacf287f12d207
Per Evensen <per.evensen@gmail.com>
2016-05-23 19:51:05 -0400

    updated confirm

--- models.rb
+++ models.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
-  before_create -> { self.confirm_token = SecureRandom.hex }
+  before_create :generate_confirm_token
   has_secure_password
   validates :email, presence: true, uniqueness: true, format: {with: /@/}
+
+  def generate_confirm_token
+    loop do
+      self.confirm_token = SecureRandom.hex
+      break unless User.find_by(confirm_token: confirm_token)
+    end
+  end
 end
