6c3c8036c7503d58b6e192fb376ba95600ce1e29
Per Evensen <per.evensen@gmail.com>
2016-06-01 17:08:38 -0400

    updated pages to display name not email. made name required

--- models.rb
+++ models.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   before_create :generate_confirm_token
   has_secure_password(validations: false)
   validates_presence_of :password, on: :create, unless: :uid
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: {with: /@/}, unless: :uid
 
   def generate_confirm_token
