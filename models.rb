36ea15839b3b871afac873e162643404a61fa07c
Per Evensen <per.evensen@gmail.com>
2016-06-05 12:16:39 -0400

    adde conditions

--- models.rb
+++ models.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  before_create :generate_confirm_token
+  before_create :generate_confirm_token, if: :has_credentials?
   has_secure_password(validations: false)
-  validates_presence_of :password, on: :create, unless: :uid
+  validates :password, :email, presence: true, on: :create, unless: :has_omni_auth?
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format: {with: /@/}, unless: :uid
+  validates :email, uniqueness: true, format: {with: /@/}
 
   def generate_confirm_token
     loop do
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
     begin
       send_confirm_email
     rescue
-      # handle error
+      # raise ActiveRecord::RecordInvalid.new(self)
     end
   end
 
@@ -26,5 +26,13 @@ class User < ActiveRecord::Base
         html_body: (haml :'mailers/confirm_registration', layout: false)
     }
     Pony.mail(Sinatra::Application.settings.pony_defaults.merge mail_options)
+  end
+
+  def has_credentials?
+    email.present? && password.present?
+  end
+
+  def has_omni_auth?
+    uid.present?
   end
 end
